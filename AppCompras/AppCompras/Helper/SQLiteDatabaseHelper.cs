d53edecdf3955ab8e2c85fccfb58ca3ef8a3a652
Usuario <Usuario@CPD02-M16>
2022-11-21 09:31:30 -0300

    Adicionar arquivos de projeto.

---
+++ AppCompras/AppCompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AppCompras.Model;
+using SQLite;
+
+namespace AppCompras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteDatabaseHelper(string path) //preparando o contrutor para quando chamarmos ele,ele abra a conexão
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto p)
+        {
+            return _connection.InsertAsync(p);
+        }
+
+        public void Update(Produto p)
+        {
+            string sql = "UPTADE produto SET descricao=?, Quantidade=?, Preco=? WHERE id= ? ";
+            _connection.QueryAsync<Produto>(sql, p.descricao, p.quantidade, p.preco, p.id);
+        }
+
+         /** public Task<Produto> getByid(int id)
+        {
+            return new Produto();
+        }
+         */
+
+       public Task<List<Produto>> GetAll()
+        {
+            return _connection.Table<Produto>().ToListAsync();
+        }
+        
+
+        public Task<int> Delete(int id)
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.id == id);
+        }
+    }
+}
---
+++ AppCompras/AppCompras/Model/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite; 
+
+namespace AppCompras.Model
+{
+    public class Produto
+    {
+        public int id { get; set; }         
+
+        public string descricao { get; set; }
+
+        public double quantidade { get; set; } 
+
+        public double preco { get; set; }
+
+    }
+}
